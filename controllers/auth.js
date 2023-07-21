b0b39bfdb3648d658a02a4cfccb6c4db49b58210
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-07-21 11:51:25 +0300

    fixed email

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,7 +33,7 @@ const register = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: "Verify Email",
-    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/$//{verificationToken}">Click verify email</a>`,
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click verify email</a>`,
   }
 
   await sendEmail(verifyEmail)
