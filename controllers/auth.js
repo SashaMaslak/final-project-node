8ca5951ba17b2839775daac2be7eb04ccd4f7868
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-07-22 13:52:11 +0300

    1

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,8 +88,14 @@ const login = async (req, res) => {
   res.json({
     token,
     user: {
+      id: user._id,
       name: user.name,
       email: user.email,
+      city: user.city,
+      phone: user.phone,
+      favorites: user.favorites,
+      ownPets: user.ownPets,
+      // avatar: user.avatar,
     },
   })
 }
