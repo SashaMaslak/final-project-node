c57654f3ed26912dc2d610d0916df2ed3cf24da1
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-07-24 11:16:59 +0300

    rm date-fns for optimization

--- helpers/transformNotice.js
+++ helpers/transformNotice.js
@@ -1,11 +1,19 @@
-const { format } = require("date-fns")
+const format = dateStr => {
+  const date = new Date(dateStr)
+  const day = String(date.getDate()).padStart(2, "0")
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const year = date.getFullYear()
+  const result = `${day}-${month}-${year}`
+  return result
+}
 
 const transformNotice = obj => {
   return {
+    _id: obj._id,
     category: obj.category,
     title: obj.title,
     name: obj.name,
-    date: format(new Date(obj.date), "dd-MM-yyyy"),
+    date: format(obj.date),
     type: obj.type,
     file: obj.file,
     sex: obj.sex,
