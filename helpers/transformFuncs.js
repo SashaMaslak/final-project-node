462a481cfd44bf7ae17d199dfa2453f14a2e4492
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-07-31 13:23:59 +0300

    small fix

--- helpers/transformFuncs.js
+++ helpers/transformFuncs.js
@@ -3,7 +3,7 @@ const format = dateStr => {
   const day = String(date.getDate()).padStart(2, "0")
   const month = String(date.getMonth() + 1).padStart(2, "0")
   const year = date.getFullYear()
-  const result = `${day}.${month}.${year}`
+  const result = `${day}-${month}-${year}`
   return result
 }
 
