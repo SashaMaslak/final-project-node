fddfc2a2ada8af8a9cdda753edb0bead92234dfc
unknown <myron.melnyk.kn.2021@lpnu.ua>
2023-08-03 17:35:57 +0300

    mentor fix

--- models/user.js
+++ models/user.js
@@ -69,26 +69,79 @@ const userSchema = new Schema(
 userSchema.post("save", handleMongooseError)
 
 const registerSchema = Joi.object({
-  name: Joi.string().min(2).max(16).required(),
-  email: Joi.string().pattern(emailRegex).required(),
-  password: Joi.string().pattern(pswRegex).min(6).max(16).required(),
+  name: Joi.string().min(2).max(16).required().messages({
+    "string.base": "The name must be a string.",
+    "string.min": "The name must be not less than 2 symbols.",
+    "string.max": "The name must be not greater than 16 symbols.",
+    "any.required": "The name field is required.",
+  }),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.base": "The email must be a string.",
+    "string.pattern.base":
+      "The email must consist of at least 1 letter at start, then @, then at least 1 letter, then a dot, at the end 2-3 letters.",
+    "any.required": "The email field is required.",
+  }),
+  password: Joi.string().pattern(pswRegex).min(6).max(16).required().messages({
+    "string.base": "The password must be a string.",
+    "string.pattern.base":
+      "The password must consist of at least one UpperCase, one LowerCase, and one digit from 6 to 16 symbols.",
+    "string.min": "The password must be not less than 6 symbols.",
+    "string.max": "The password must be not greater than 16 symbols.",
+    "any.required": "The password field is required.",
+  }),
 })
 
 const emailSchema = Joi.object({
-  email: Joi.string().pattern(emailRegex).required(),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.base": "The email must be a string.",
+    "string.pattern.base":
+      "The email must consist of at least 1 letter at start, then @, then at least 1 letter, then a dot, at the end 2-3 letters.",
+    "any.required": "The email field is required.",
+  }),
 })
 
 const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegex).required(),
-  password: Joi.string().min(6).max(16).required(),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.base": "The email must be a string.",
+    "string.pattern.base":
+      "The email must consist of at least 1 letter at start, then @, then at least 1 letter, then a dot, at the end 2-3 letters.",
+    "any.required": "The email field is required.",
+  }),
+  password: Joi.string().min(6).max(16).required().messages({
+    "string.base": "The password must be a string.",
+    "string.min": "The password must be not less than 6 symbols.",
+    "string.max": "The password must be not greater than 16 symbols.",
+    "any.required": "The password field is required.",
+  }),
 })
 
 const updateSchema = Joi.object({
-  name: Joi.string().min(2).max(16).required(),
-  email: Joi.string().pattern(emailRegex).required(),
-  phone: Joi.string().pattern(phoneRegex),
-  birthday: Joi.string().pattern(dateRegex),
-  city: Joi.string().min(2).pattern(cityRegex),
+  name: Joi.string().min(2).max(16).required().messages({
+    "string.base": "The name must be a string.",
+    "string.min": "The name must be not less than 2 symbols.",
+    "string.max": "The name must be not greater than 16 symbols.",
+    "any.required": "The name field is required.",
+  }),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.base": "The email must be a string.",
+    "string.pattern.base":
+      "The email must consist of at least 1 letter at start, then @, then at least 1 letter, then a dot, at the end 2-3 letters.",
+    "any.required": "The email field is required.",
+  }),
+  phone: Joi.string().pattern(phoneRegex).messages({
+    "string.base": "The phone must be a string.",
+    "string.pattern.base": "The phone must be in format +380XXXXXXXXX.",
+  }),
+  birthday: Joi.string().pattern(dateRegex).messages({
+    "string.base": "The birthday must be a string.",
+    "string.pattern.base": "birthday phone must be in format DD-MM-YYYY.",
+  }),
+  city: Joi.string().min(2).pattern(cityRegex).messages({
+    "string.base": "The city must be a string.",
+    "string.min": "The city must be not less than 2 symbols.",
+    "string.pattern.base":
+      "The city must consist of only letters, no numbers and no spaces and have at least 2 symbols.",
+  }),
 })
 
 const schemas = {
